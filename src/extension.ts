f611b4e92b101caa1e6f564d8f0ab0d918607ab0
Caleb Denio <cjdenio44@gmail.com>
2021-06-10 21:58:03 -0400

    ✨ Script loading

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as fs from "fs";
+import { join } from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "codescript" is now active!');
+  const searchPaths = vscode.workspace
+    .getConfiguration("codescript")
+    .get("searchPaths") as string[];
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "codescript.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
+  Object.assign(global, {
+    c: {
+      info: vscode.window.showInformationMessage,
+      error: vscode.window.showErrorMessage,
+      onFileOpened(
+        callback: (d: vscode.TextDocument) => any,
+        filetype?: string
+      ) {
+        vscode.workspace.onDidOpenTextDocument((d) => {
+          if (!filetype || d.languageId === filetype) {
+            callback(d);
+          }
+        });
+      },
+    },
+  });
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from codescript!");
-    }
-  );
+  searchPaths.forEach((p) => {
+    fs.readdir(p, { withFileTypes: true }, (err, files) => {
+      if (err !== null) {
+        vscode.window.showErrorMessage(`Error loading script path: ${p}`);
+        return;
+      }
 
-  context.subscriptions.push(disposable);
+      files.forEach((file) => {
+        if (file.isFile() && /\.js$/.test(file.name)) {
+          import(join(p, file.name)).catch((e) => {
+            console.log("error");
+          });
+        }
+      });
+    });
+  });
+
+  // let disposable = vscode.commands.registerCommand(
+  //   "codescript.helloWorld",
+  //   async () => {
+  //     Object.assign(global, {
+  //       c: {
+  //         info: vscode.window.showInformationMessage,
+  //       },
+  //     });
+
+  //     const script = "/Users/calebdenio/.config/codescript/index.js";
+
+  //     await import(script);
+  //   }
+  // );
+
+  // context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
